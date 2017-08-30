d41b4f692765bc97e43dacccc7f82501f2d11ca5
Brian Mont <briansmont830@gmail.com>
2017-08-30 20:06:11 +0000

    created method for topics create, styled new page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,7 +7,24 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
   end
+
+  def created
+    @topic = Topic.new
+    @topic.title = params[:topic][:title]
+    @topic.body = params[:topic][:body]
+    
+    if @topic.save
+      flash[:notice] = "Topic was created, thanks!"
+      redirect_to @topic
+    else 
+      flash.now[:alert] = "Error creating topic :( please try again."
+      render :new
+    
+    end
+  end
+
 
   def edit
   end
