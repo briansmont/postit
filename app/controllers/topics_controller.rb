003572dbb32e886a2044b49d37539b90d201cb60
Brian Mont <briansmont830@gmail.com>
2017-08-31 14:12:18 +0000

    topics can be updated, reflects in the view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -28,5 +28,23 @@ class TopicsController < ApplicationController
 
 
   def edit
+    @topic = Topic.find(params[:id])
   end
+  
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.title = params[:topic][:title]
+    @topic.body = params[:topic][:body]
+    
+    if @topic.save
+      flash[:notice] = "Topic details have been updated."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "There was an error updating the topic, please try again."
+      render :edit
+    end
+      
+  end
+  
+  
 end
