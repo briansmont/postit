a5c376987491cfe969935a5b943952335c13206b
Brian Mont <briansmont830@gmail.com>
2017-09-01 14:19:42 +0000

    partial for new & edit topics, also ability to show WHAT ERRORS SPECIFICALLY for each new or edit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  
 end
